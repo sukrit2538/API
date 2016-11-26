91da678b3d8cd1e45967a2943f8d1666d81129f1
sukritnoom <sukrit424@gmail.com>
2016-11-26 15:46:21 +0700

    simple server

---
+++ server.go
@@ -0,0 +1,12 @@
+package main
+import (
+	"net/http"
+	"github.com/labstack/echo"
+)
+func main(){
+  e := echo.New()
+  e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+  })
+    e.Logger.Fatal(e.Start(":1323"))
+}
